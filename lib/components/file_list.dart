98687ae168a8b525fb92d2e913c3f1d92a21f026
zhoucheng <zhoucheng133@outlook.com>
2024-11-01 21:56:17 +0800

    修改文件命名

--- lib/components/file_list.dart
+++ lib/components/file_list.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:ffmpeg_gui/components/file_preview.dart';
+import 'package:ffmpeg_gui/components/file_item.dart';
 import 'package:ffmpeg_gui/service/variables.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
