cfb692975ba172661a783b1a1aa6d6051b075b06
zhoucheng <zhoucheng133@outlook.com>
2024-11-03 00:39:06 +0800

    添加配置页

--- lib/components/config_panel.dart
+++ lib/components/config_panel.dart
@@ -1,4 +1,8 @@
+import 'package:ffmpeg_gui/service/variables.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:path/path.dart' as p;
 
 class ConfigPanel extends StatefulWidget {
   const ConfigPanel({super.key});
@@ -8,8 +12,35 @@ class ConfigPanel extends StatefulWidget {
 }
 
 class _ConfigPanelState extends State<ConfigPanel> {
+  
+  final Controller c = Get.put(Controller());
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10)
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Obx(()=>
+          c.fileList.isNotEmpty ? 
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                p.basename(c.fileList[c.selectIndex.value].path),
+                maxLines: 2,
+                style: GoogleFonts.notoSansSc(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ],
+          ) : Container()
+        ),
+      ),
+    );
   }
 }
