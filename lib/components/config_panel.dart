a10493dd18b6ed33bc3e96b105ccbf48c9afdec3
zhoucheng <zhoucheng133@outlook.com>
2024-11-03 18:34:07 +0800

    添加选择编码

--- lib/components/config_panel.dart
+++ lib/components/config_panel.dart
@@ -1,5 +1,6 @@
+import 'package:ffmpeg_gui/service/task_item.dart';
 import 'package:ffmpeg_gui/service/variables.dart';
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:path/path.dart' as p;
@@ -36,7 +37,83 @@ class _ConfigPanelState extends State<ConfigPanel> {
                   fontSize: 16,
                   fontWeight: FontWeight.bold
                 ),
-              )
+              ),
+              const SizedBox(height: 10,),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 90,
+                    child: Text(
+                      '完整路径',
+                      style: GoogleFonts.notoSansSc(),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      c.fileList[c.selectIndex.value].path
+                    )
+                  )
+                ],
+              ),
+              const SizedBox(height: 10,),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 90,
+                    child: Text(
+                      '编码',
+                      style: GoogleFonts.notoSansSc(),
+                    ),
+                  ),
+                  ComboBox(
+                    value: c.fileList[c.selectIndex.value].encoder,
+                    items: [
+                      ComboBoxItem(
+                        value: Encoders.libx264,
+                        enabled: c.fileList[c.selectIndex.value].type==Types.video,
+                        child: Text('libx264', style: GoogleFonts.notoSansSc(),),
+                      ),
+                      ComboBoxItem(
+                        value: Encoders.libx265,
+                        enabled: c.fileList[c.selectIndex.value].type==Types.video,
+                        child: Text('libx265', style: GoogleFonts.notoSansSc(),),
+                      ),
+                      ComboBoxItem(
+                        value: Encoders.libaomav1,
+                        enabled: c.fileList[c.selectIndex.value].type==Types.video,
+                        child: Text('libaomav1', style: GoogleFonts.notoSansSc(),),
+                      ),
+                      ComboBoxItem(
+                        value: Encoders.libxvid,
+                        enabled: c.fileList[c.selectIndex.value].type==Types.video,
+                        child: Text('libxvid', style: GoogleFonts.notoSansSc(),),
+                      ),
+                      ComboBoxItem(
+                        value: Encoders.aac,
+                        enabled: c.fileList[c.selectIndex.value].type==Types.audio,
+                        child: Text('aac', style: GoogleFonts.notoSansSc(),),
+                      ),
+                      ComboBoxItem(
+                        value: Encoders.libmp3lame,
+                        enabled: c.fileList[c.selectIndex.value].type==Types.audio,
+                        child: Text('libmp3lame', style: GoogleFonts.notoSansSc(),),
+                      ),
+                      ComboBoxItem(
+                        value: Encoders.flac,
+                        enabled: c.fileList[c.selectIndex.value].type==Types.audio,
+                        child: Text('flac', style: GoogleFonts.notoSansSc(),),
+                      )
+                    ],
+                    onChanged: (value){
+                      if(value!=null){
+                        c.fileList[c.selectIndex.value].encoder=value;
+                      }
+                    },
+                  ),
+                ],
+              ),
             ],
           ) : Container()
         ),
