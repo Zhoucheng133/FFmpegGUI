7ca6c309a159943a8a2606fe0b894b9f920ec11c
zhoucheng <zhoucheng133@outlook.com>
2024-12-27 19:01:44 +0800

    更新版本号

--- lib/service/variables.dart
+++ lib/service/variables.dart
@@ -10,6 +10,6 @@ class Controller extends GetxController{
   RxInt selectIndex=0.obs;
   RxString output=''.obs;
 
-  String version='0.3.5';
+  String version='0.3.6';
   RxString ffmpeg="ffmpeg".obs;
 }
