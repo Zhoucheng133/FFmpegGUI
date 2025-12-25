73a79818eb4f0acfd453ac5fa595993a7af84f9a
zhoucheng <zhoucheng133@outlook.com>
2025-12-25 23:21:39 +0800

    修复一个窗口显示问题

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -5,9 +5,9 @@ import window_manager
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
     self.backgroundColor = NSColor.clear
     flutterViewController.backgroundColor = NSColor.clear
+    let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
