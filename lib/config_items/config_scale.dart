79d619fac095286e4b379784008e6c78e2459539
zhoucheng <zhoucheng133@outlook.com>
2026-03-20 04:16:22 +0800

    修复一个错误

--- lib/config_items/config_scale.dart
+++ lib/config_items/config_scale.dart
@@ -29,101 +29,103 @@ class _ConfigScaleState extends State<ConfigScale> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: .min,
-      crossAxisAlignment: .start,
-      children: [
-        Row(
-          crossAxisAlignment: .center, 
-          children: [
-            SizedBox(
-              width: 90,
-              child: Text('scale'.tr),
-            ),
-            Dropdown(
-              enable: controller.selectedTask.videoEncoders!=VideoEncoders.copy,
-              value: controller.selectedTask.sizeSelected(), 
-              onChanged: (val){
-                if(val=='noScale'.tr){
-                  controller.fileList[controller.selectIndex.value].customSize=false;
-                  controller.fileList[controller.selectIndex.value].width=null;
-                  controller.fileList[controller.selectIndex.value].height=null;
-                }else if(val=='custom'.tr){
-                  controller.fileList[controller.selectIndex.value].customSize=true;
-                  widthController.text="1920";
-                  heightController.text="1080";
-                }else{
-                  controller.fileList[controller.selectIndex.value].customSize=false;
-                  if(val=='1080p'){
-                    controller.fileList[controller.selectIndex.value].width=1920;
-                    controller.fileList[controller.selectIndex.value].height=1080;
-                  }else if(val=='720p'){
-                    controller.fileList[controller.selectIndex.value].width=1280;
-                    controller.fileList[controller.selectIndex.value].height=720;
-                  }
-                }
-                controller.fileList.refresh();
-              },
-              items: scaleItems.map((val)=>DropdownListItem(
-                label: val, 
-                value: val
-              )).toList()
-            )
-          ],
-        ),
-        if(controller.fileList[controller.selectIndex.value].customSize==true) Padding(
-          padding: .only(top: 10, left: 95),
-          child: Row(
+    return Obx(
+      ()=> Column(
+        mainAxisSize: .min,
+        crossAxisAlignment: .start,
+        children: [
+          Row(
+            crossAxisAlignment: .center, 
             children: [
               SizedBox(
-                width: 100,
-                child: TextField(
-                  controller: widthController,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly,
-                  ],
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    isCollapsed: true,
-                    contentPadding: .symmetric(vertical: 10, horizontal: 5)
-                  ),
-                  onChanged: (val){
-                    try{
-                      controller.fileList[controller.selectIndex.value].width=int.parse(val);
-                    }catch(_){}
-                  },
-                ),
+                width: 90,
+                child: Text('scale'.tr),
               ),
-              Padding(
-                padding: .symmetric(horizontal: 10),
-                child: FaIcon(
-                  FontAwesomeIcons.xmark,
-                  size: 20,
-                )
-              ),
-              SizedBox(
-                width: 100,
-                child: TextField(
-                  controller: heightController,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly,
-                  ],
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    isCollapsed: true,
-                    contentPadding: .symmetric(vertical: 10, horizontal: 5)
-                  ),
-                  onChanged: (val){
-                    try{
-                      controller.fileList[controller.selectIndex.value].height=int.parse(val);
-                    }catch(_){}
-                  },
-                ),
+              Dropdown(
+                enable: controller.selectedTask.videoEncoders!=VideoEncoders.copy,
+                value: controller.selectedTask.sizeSelected(), 
+                onChanged: (val){
+                  if(val=='noScale'.tr){
+                    controller.fileList[controller.selectIndex.value].customSize=false;
+                    controller.fileList[controller.selectIndex.value].width=null;
+                    controller.fileList[controller.selectIndex.value].height=null;
+                  }else if(val=='custom'.tr){
+                    controller.fileList[controller.selectIndex.value].customSize=true;
+                    widthController.text="1920";
+                    heightController.text="1080";
+                  }else{
+                    controller.fileList[controller.selectIndex.value].customSize=false;
+                    if(val=='1080p'){
+                      controller.fileList[controller.selectIndex.value].width=1920;
+                      controller.fileList[controller.selectIndex.value].height=1080;
+                    }else if(val=='720p'){
+                      controller.fileList[controller.selectIndex.value].width=1280;
+                      controller.fileList[controller.selectIndex.value].height=720;
+                    }
+                  }
+                  controller.fileList.refresh();
+                },
+                items: scaleItems.map((val)=>DropdownListItem(
+                  label: val, 
+                  value: val
+                )).toList()
               )
             ],
           ),
-        )
-      ],
+          if(controller.fileList[controller.selectIndex.value].customSize==true) Padding(
+            padding: .only(top: 10, left: 95),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: TextField(
+                    controller: widthController,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      isCollapsed: true,
+                      contentPadding: .symmetric(vertical: 10, horizontal: 5)
+                    ),
+                    onChanged: (val){
+                      try{
+                        controller.fileList[controller.selectIndex.value].width=int.parse(val);
+                      }catch(_){}
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: .symmetric(horizontal: 10),
+                  child: FaIcon(
+                    FontAwesomeIcons.xmark,
+                    size: 20,
+                  )
+                ),
+                SizedBox(
+                  width: 100,
+                  child: TextField(
+                    controller: heightController,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      isCollapsed: true,
+                      contentPadding: .symmetric(vertical: 10, horizontal: 5)
+                    ),
+                    onChanged: (val){
+                      try{
+                        controller.fileList[controller.selectIndex.value].height=int.parse(val);
+                      }catch(_){}
+                    },
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
